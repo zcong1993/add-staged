537d42cd061eefd1822a264eae1f608b4776760e
zcong1993 <zhangcong1992@gmail.com>
2022-04-02 00:57:45 +0800

    feat: finished

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/urfave/cli/v2"
 )
@@ -17,17 +21,13 @@ var (
 
 func main() {
 	cli.VersionPrinter = func(c *cli.Context) {
-		fmt.Println(buildVersion(version, commit, date, builtBy)) //nolint
+		fmt.Println(buildVersion(version, commit, date, builtBy))
 	}
 
 	app := &cli.App{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
-		Action: func(c *cli.Context) error {
-			fmt.Println("boom! I say!") //nolint
-
-			return nil
-		},
+		Name:    "add-staged",
+		Usage:   "git add only staged files",
+		Action:  addStaged,
 		Version: version,
 	}
 
@@ -35,6 +35,43 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func addStaged(c *cli.Context) error {
+	files, err := getStagedFiles(mustCwd())
+	if err != nil {
+		return fmt.Errorf("get staged files err %v", err)
+	}
+
+	args := []string{"add", "--"}
+	args = append(args, files...)
+
+	return exec.Command("git", args...).Run()
+}
+
+func mustCwd() string {
+	cwd, _ := os.Getwd()
+	return cwd
+}
+
+func getStagedFiles(cwd string) ([]string, error) {
+	cmd := exec.Command("git", "diff", "--staged", "--diff-filter=ACMR", "--name-only", "-z")
+	cmd.Dir = cwd
+	resp, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	r1 := regexp.MustCompile("\u0000$")
+	resp = r1.ReplaceAll(resp, []byte(""))
+
+	files := strings.Split(string(resp), "\u0000")
+	nfs := make([]string, len(files))
+	for i, f := range files {
+		nfs[i] = filepath.Join(cwd, f)
+	}
+
+	return nfs, nil
 }
 
 func buildVersion(version, commit, date, builtBy string) string {
